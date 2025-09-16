1aefa8f31f709d0292d709a8dc35133c8d3dc15f
urjj <jjplayz2winn@gmail.com>
2025-09-16 17:18:19 -0500

    Initial commit

---
+++ src/main/java/net/urjj/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.urjj.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
