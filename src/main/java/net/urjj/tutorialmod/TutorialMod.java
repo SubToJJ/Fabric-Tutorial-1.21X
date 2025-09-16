680df111618c83db19626e0985c667d5322954fa
urjj <jjplayz2winn@gmail.com>
2025-09-16 17:39:24 -0500

    glowall command

--- src/main/java/net/urjj/tutorialmod/TutorialMod.java
+++ src/main/java/net/urjj/tutorialmod/TutorialMod.java
@@ -1,16 +1,23 @@
 package net.urjj.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // very important comment
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing TutorialMod...");
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            GlowCommand.register(dispatcher, registryAccess);
+            LOGGER.info("Registered /glowall command");
+        });
+    }
 }
